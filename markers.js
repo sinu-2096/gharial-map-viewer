fe13e84302758cd263892351507818c2d09d8203
sinu-2096 <162143981+sinu-2096@users.noreply.github.com>
2026-03-15 15:25:20 +0530

    Update markers.js

--- markers.js
+++ markers.js
@@ -1,32 +1,21 @@
 function createMarker(record){
 
-const lat=parseFloat(record.Latitude);
-const lon=parseFloat(record.Longitude);
-
-if(!lat||!lon) return;
+const lat=parseFloat(record["Latitude"]);
+const lon=parseFloat(record["Longitude"]);
 
 const id=record["Animal ID"];
 
-const dead=CONFIG.RED_IDS.includes(parseInt(id));
-
-const color=dead?"red":"blue";
-
 const marker=L.circleMarker([lat,lon],{
-radius:8,
-color:color,
+radius:7,
+color:"#007bff",
 fillOpacity:0.8
 });
 
-marker.bindPopup(`
-<b>ID:</b> ${id}<br>
-<b>Time:</b> ${record.Timestamp}<br>
-<b>Place:</b> ${record.Place}<br>
-<b>Activity:</b> ${record.Activity}<br>
-<b>Signal:</b> ${record["Signal Strength"]}<br>
-<b>Lat:</b> ${lat}<br>
-<b>Lon:</b> ${lon}<br>
-<b>Remarks:</b> ${record.Remarks}
-`);
+marker.bindPopup(
+"<b>ID:</b> "+id+
+"<br><b>Place:</b> "+record["Place"]+
+"<br><b>Activity:</b> "+record["Activity"]
+);
 
 marker.addTo(markersLayer);
 
